e76547a747a142dbb4e87041f7127795229ded5e
Mouli2105 <sreekar.mouli1998@gmail.com>
2019-01-13 16:37:57 +0530

    Modified auth response.

--- Tool/views/auth.py
+++ Tool/views/auth.py
@@ -4,5 +4,8 @@ def jwt_response_payload_handler(token, user=None, request=None):
     This function controlls the custom payload after login or token refresh. This data is returned through the web API.
     """
     return {
-        'token': token,
+        'token'     : token,
+        'is_student': user.is_student,
+        'is_mentor' : user.is_mentor,
+        'username'  : user.username
     }
