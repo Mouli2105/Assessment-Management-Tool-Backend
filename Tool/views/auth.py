345af4e3a9b614eea588f7097f9c58fd9fb910a8
mouli2105 <sreekar.mouli1998@gmail.com>
2019-03-30 23:26:50 +0530

    Added userId to auth response

--- Tool/views/auth.py
+++ Tool/views/auth.py
@@ -3,9 +3,14 @@ def jwt_response_payload_handler(token, user=None, request=None):
     Custom response payload handler.
     This function controlls the custom payload after login or token refresh. This data is returned through the web API.
     """
-    return {
+    responseObj = {
         'token'     : token,
         'is_student': user.is_student,
         'is_mentor' : user.is_mentor,
         'username'  : user.username
     }
+    if user.is_student:
+        responseObj['user_id'] = user.student.id
+    elif user.is_mentor:
+        responseObj['user_id'] = user.mentor.id
+    return responseObj
